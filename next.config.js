bc1139ec7cd6e2b3788e42f3bd9ab134b5a4528c
Sanika Surose <109695485+sanikasurose@users.noreply.github.com>
2026-02-20 16:46:57 -0500

    Troubleshoot Github Pages build failure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/personal-website" : "",
+  assetPrefix: isProd ? "/personal-website/" : "",
   images: {
     unoptimized: true,
   },
